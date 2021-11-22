cc00059426e25f9d2805142bdbe8c93e84699c5a
Pius E. Uduma <41404729+controlpee@users.noreply.github.com>
2021-11-22 15:06:42 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import { CssBaseline } from '@material-ui/core';
 import './App.css';
-// import Appbar from './layouts/Main';
 import Header from './layouts/Header/Header';
 function App() {
 
@@ -10,9 +9,6 @@ function App() {
     <CssBaseline />
       <Router>
         <Header />
-      <Routes>
-      {/* <Route exact path='/' render={(props) => <Appbar/>} /> */}
-      </Routes>
       </Router>
     </div>
   );
