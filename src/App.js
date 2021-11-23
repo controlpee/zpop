bd5e195a68ab38ebf0a368d95aa8ea9286f879ac
Pius E. Uduma <41404729+controlpee@users.noreply.github.com>
2021-11-23 10:11:49 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,14 +2,6 @@ import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import { CssBaseline } from '@material-ui/core';
 import './App.css';
 import Header from './layouts/Header/Header';
-import Dashboard from './components/Dashboard';
-import Sell from './components/Sell';
-import Reporting from './components/Reporting';
-import SalesLedger from './components/SalesLedger';
-import Settings from './components/Settings';
-import Products from './components/Products'
-import Customers from './components/Customers';
-import Ecommerce from './components/Ecommerce';
 
 
 function App() {
